d30f2cf4e6e663badb26fe2b360bd1ea88e4902f
tylersanderson <tylersanderson@hotmail.com>
2019-10-20 16:32:24 -0500

    adding docker branch

--- server.js
+++ server.js
@@ -8,26 +8,26 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const auth = require('./controllers/authorization');
 
 const db = knex({
   client: 'pg',
-  connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true,
-  }
+  connection: process.env.POSTGRES_URI
 });
 
 const app = express();
+console.log('checkcheck')
 
 app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/', (req, res)=> { res.send('it is working!') })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => {image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res, db)})
+app.put('/image', auth.requireAuth, (req, res) => {image.handleImage(req, res, db)})
+app.post('/imageurl', auth.requireAuth, (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(process.env.PORT || 3000, ()=> {
 	console.log('app is running on port ${process.env.PORT}');
